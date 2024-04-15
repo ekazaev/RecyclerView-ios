f24da6e675ed9a054dd41278908dc29d64fbd6ce
Eugene Kazaev <eugene.kazaev@gmail.com>
2024-04-15 17:22:31 +0100

    Attempt to host package

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let version = "0.1.23"
+let moduleName = "RecyclerView"
+let checksum = "d1ae8e2909349cd3e52e987d5d8c9df5cfc0ff661f0e1997d49f205ab08477f4"
+
+let package = Package(
+    name: moduleName,
+    platforms: [
+        .iOS(.v15),
+        .macCatalyst(.v15),
+        .visionOS(.v1)
+    ],
+    products: [
+        .library(
+            name: moduleName,
+            targets: [moduleName]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: moduleName,
+            url: "https://github.com/ekazaev/RecyclerView-ios/Distribution/\(version)/\(moduleName).xcframework.zip",
+            checksum: checksum
+        )
+    ]
+)
--- RecyclerView.podspec
+++ RecyclerView.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name         = "RecyclerView"
-    s.version      = "0.1.22"
+    s.version      = "0.1.23"
     s.summary      = "A brief description of RecyclerView project."
     s.description  = <<-DESC
     An extended description of RecyclerView project.
